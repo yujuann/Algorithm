3c8638dd1097abec793589c3a5cc66cea9c3939c
yuju <yujuann@github.com>
2022-11-30 23:59:24 +0900

    3slove

---
+++ 프로그래머스/LV.0/두 수의 곱.js
@@ -0,0 +1,18 @@
+function solution(num1, num2) {
+  return num1 * num2;
+}
+
+// Another way To Slove
+// 1. const solution = (num1, num2) => num1 * num2
+
+//  2.이중 for 문
+//  function solution(num1, num2) {
+//    var ans =0 ;
+//    for (let i  = 1; i<=num1; i++){
+//      for (let j=1; j<= num2; j++){
+//        ans = ans + 1;
+//      }
+//    }
+
+// return ans;
+//  }
