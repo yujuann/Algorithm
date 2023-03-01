2b91909a6e6c77e4b46dd33cbb682675bfd4f45a
yujuann <xxxnewzu@gmail.com>
2023-03-01 14:17:36 +0900

    filter , includes , length

---
+++ 프로그래머스/LV.0/배열의 유사도.js
@@ -0,0 +1,7 @@
+const solution = (s1, s2) => s1.filter((a, i) => s2.includes(a)).length;
+
+// filter -> 배열의 내용을 특정 조건에
+// includes -> 배열에서 특정값을  포함하는 것
+
+// length -> 문자열의 길이를 반환하는 속성입니다
+// string.length
