ec4f90956379541671a821a05e9ab52ece3a5952
yujuann <xxxnewzu@gmail.com>
2023-03-09 17:26:12 +0900

    filter

--- 프로그래머스/LV.0/자릿수 더하기.js
+++ 프로그래머스/LV.0/자릿수 더하기.js
@@ -4,3 +4,13 @@ function solution(n) {
     .split("")
     .reduce((acc, cur) => acc + Number(cur), 0);
 }
+
+
+문자로 바꿀지  <-> 숫자로 바꿀지 
+
+Number / ParseInt / +n /n +''/ n.toString /
+(n + '') .split
+주어진 값으 n 이 숫자라서 
+1. 숫자를 문자열로 바꿔야한다.
+2. split 사용하기
+3. 리듀스 사용해서 반복해서  다시 문자열을 숫자로 바꾸기 
